24aee827c168155686a36d0e55e8f44424a6dd77
Data-drone <bpl.law@gmail.com>
2021-04-20 05:16:04 +0000

    refactoring the scripts to split out the feature engineering steps

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,92 +1,143 @@
 ### Data Engineering Job
 
-### Set conda env?
-#%conda activate rapids-0.18
-
-import cudf
 import numpy as np
+import cudf as dd
 
 ## activate auto memory management
 ## allows for spill
-cudf.set_allocator("managed")
-
-### Load datasets
-bureau_balance = cudf.read_csv('data/bureau_balance.csv')
-bureau = cudf.read_csv('data/bureau.csv')
-cc_balance = cudf.read_csv('data/credit_card_balance.csv')
-payments = cudf.read_csv('data/installments_payments.csv')
-pc_balance = cudf.read_csv('data/POS_CASH_balance.csv')
-prev = cudf.read_csv('data/previous_application.csv')
-train = cudf.read_csv('data/application_train.csv')
-test = cudf.read_csv('data/application_test.csv')
-
-## aggregation functions for our groupings
-agg_func = ['mean', 'max', 'min', 'sum', 'std']
-
-## Build Average Bureau Balance
-avg_bbalance = bureau_balance.select_dtypes('number') \
-                .groupby('SK_ID_BUREAU').agg(agg_func)
-
-avg_bbalance.columns = ["_".join(x) for x in avg_bbalance.columns.ravel()]
-
-## Build sum Credit Card Balance
-sum_cc_balance = cc_balance.drop('SK_ID_PREV', axis=1) \
-                    .select_dtypes('number').groupby('SK_ID_CURR') \
-                    .agg(agg_func)
-
-sum_cc_balance.columns = ["_".join(x) for x in sum_cc_balance.columns.ravel()]
-
-## Build Avg Bureau table
-avg_bureau = bureau.merge(avg_bbalance, how='left', 
-                          left_on='SK_ID_BUREAU', 
-                          right_index=True)
-
-## Buld Payments
-sum_payments = payments.drop('SK_ID_PREV', axis=1)
-sum_payments['PAYMENT_PERC'] = sum_payments.AMT_PAYMENT / sum_payments.AMT_INSTALMENT
-sum_payments['PAYMENT_DIFF'] = sum_payments.AMT_INSTALMENT - sum_payments.AMT_PAYMENT
-sum_payments['DPD'] = sum_payments.DAYS_ENTRY_PAYMENT - sum_payments.DAYS_INSTALMENT
-sum_payments['DBD'] = sum_payments.DAYS_INSTALMENT - sum_payments.DAYS_ENTRY_PAYMENT
-sum_payments['DPD'] = sum_payments['DPD']
-sum_payments['DBD'] = sum_payments['DBD']
-
-## Build Sum_PC_Balance
-sum_pc_balance = pc_balance.drop('SK_ID_PREV', axis=1).select_dtypes('number').groupby('SK_ID_CURR') \
-            .agg(agg_func)
-
-sum_pc_balance.columns = ["_".join(x) for x in sum_pc_balance.columns.ravel()]
-
-## Build Sum_Prev
-prev = prev.drop('SK_ID_PREV', axis=1)
-prev.DAYS_FIRST_DRAWING = prev.DAYS_FIRST_DRAWING.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_FIRST_DUE = prev.DAYS_FIRST_DUE.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_LAST_DUE_1ST_VERSION = prev.DAYS_LAST_DUE_1ST_VERSION.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_LAST_DUE = prev.DAYS_LAST_DUE.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_TERMINATION = prev.DAYS_TERMINATION.map(lambda x: np.nan if x == 365243 else x)
-prev.APP_CREDIT_PERC = prev.AMT_APPLICATION / prev.AMT_CREDIT
-
-sum_prev = prev.select_dtypes('number').groupby('SK_ID_CURR') \
-            .agg(agg_func)
-
-sum_prev.columns = ["_".join(x) for x in sum_prev.columns.ravel()]
-
-train_feat = train.drop('TARGET', axis=1) \
-    .merge(avg_bureau, how='left', left_on='SK_ID_CURR', right_on='SK_ID_CURR') \
-    .merge(sum_cc_balance, how='left', left_on='SK_ID_CURR', right_index=True) \
-    .merge(sum_payments, how='left', left_on='SK_ID_CURR', right_on='SK_ID_CURR') \
-    .merge(sum_pc_balance, how='left', left_on='SK_ID_CURR', right_index=True) \
-    .merge(sum_prev, how='left', left_on='SK_ID_CURR', right_index=True) \
-    .drop('SK_ID_CURR', axis=1)
-
-train_feat['DAYS_EMPLOYED'] = train_feat.DAYS_EMPLOYED.map(lambda x: np.nan if x == 365243 else x)
-train_feat['DAYS_EMPLOYED_PERC'] = np.sqrt(train_feat.DAYS_EMPLOYED / train_feat.DAYS_BIRTH)
-train_feat['INCOME_CREDIT_PERC'] = train_feat.AMT_INCOME_TOTAL / train_feat.AMT_CREDIT
-train_feat['INCOME_PER_PERSON'] = np.log1p(train_feat.AMT_INCOME_TOTAL / train_feat.CNT_FAM_MEMBERS)
+#cudf.set_allocator("managed")
 
 
-## Outputs - to write out
-#avg_bureau.to_pandas().to_parquet(path='data_eng/avg_bureau.parq')
-#sum_cc_balance.to_pandas().to_parquet(path='data_eng/sum_cc_balance.parq')
-#sum_payments.to_pandas().to_parquet(path='data_eng/sum_payments.parq')
-#sum_pc_balance.to_pandas().to_parquet(path='data_eng/sum_pc_balance.parq')
-#sum_prev.to_pandas().to_parquet(path='data_eng/sum_prev.parq')
+def feature_engineering(bureau_balance, bureau, cc_balance, payments, pc_balance,
+                        prev, train, test):
+    """
+
+    Feature engineering script to process our data
+    we split this into it's own function so that we can use it across 
+    different functions easier
+
+    """
+
+    ## aggregation functions for our groupings
+    agg_func = ['mean', 'max', 'min', 'sum', 'std']
+
+    print("procecssing bureau balance")
+
+    ## Build Average Bureau Balance
+    avg_bbalance = bureau_balance.select_dtypes('number') \
+                    .groupby('SK_ID_BUREAU').agg(agg_func)
+
+    avg_bbalance.columns = ["_".join(x) for x in avg_bbalance.columns.ravel()]
+
+    # free up gpu ram
+    del(bureau_balance)
+
+    print("procecssing cc balance")
+
+    ## Build sum Credit Card Balance
+    sum_cc_balance = cc_balance.drop('SK_ID_PREV', axis=1) \
+                        .select_dtypes('number').groupby('SK_ID_CURR') \
+                        .agg(agg_func)
+
+    sum_cc_balance.columns = ["_".join(x) for x in sum_cc_balance.columns.ravel()]
+
+    # free up gpu ram
+    del(cc_balance)
+
+    print("procecssing bureau")
+
+    ## Build Avg Bureau table
+    avg_bureau = bureau.merge(avg_bbalance, how='left', 
+                              left_on='SK_ID_BUREAU', 
+                              right_index=True)
+
+    ## free up gpu ram
+    del(bureau)
+    del(avg_bbalance)
+
+    print("procecssing payments")
+
+    ## Buld Payments
+    sum_payments = payments.drop('SK_ID_PREV', axis=1)
+    sum_payments['PAYMENT_PERC'] = sum_payments.AMT_PAYMENT / sum_payments.AMT_INSTALMENT
+    sum_payments['PAYMENT_DIFF'] = sum_payments.AMT_INSTALMENT - sum_payments.AMT_PAYMENT
+    sum_payments['DPD'] = sum_payments.DAYS_ENTRY_PAYMENT - sum_payments.DAYS_INSTALMENT
+    sum_payments['DBD'] = sum_payments.DAYS_INSTALMENT - sum_payments.DAYS_ENTRY_PAYMENT
+    sum_payments['DPD'] = sum_payments['DPD']
+    sum_payments['DBD'] = sum_payments['DBD']
+
+    del(payments)
+
+    print("processing pc_balance")
+
+    ## Build Sum_PC_Balance
+    sum_pc_balance = pc_balance.drop('SK_ID_PREV', axis=1).select_dtypes('number').groupby('SK_ID_CURR') \
+                .agg(agg_func)
+
+    sum_pc_balance.columns = ["_".join(x) for x in sum_pc_balance.columns.ravel()]
+
+    # free up gpu ram
+    del(pc_balance)
+
+    print("processing prev table")
+
+    ## Build Sum_Prev
+    prev = prev.drop('SK_ID_PREV', axis=1)
+    prev.DAYS_FIRST_DRAWING = prev.DAYS_FIRST_DRAWING.map(lambda x: np.nan if x == 365243 else x)
+    prev.DAYS_FIRST_DUE = prev.DAYS_FIRST_DUE.map(lambda x: np.nan if x == 365243 else x)
+    prev.DAYS_LAST_DUE_1ST_VERSION = prev.DAYS_LAST_DUE_1ST_VERSION.map(lambda x: np.nan if x == 365243 else x)
+    prev.DAYS_LAST_DUE = prev.DAYS_LAST_DUE.map(lambda x: np.nan if x == 365243 else x)
+    prev.DAYS_TERMINATION = prev.DAYS_TERMINATION.map(lambda x: np.nan if x == 365243 else x)
+    prev.APP_CREDIT_PERC = prev.AMT_APPLICATION / prev.AMT_CREDIT
+
+    sum_prev = prev.select_dtypes('number').groupby('SK_ID_CURR') \
+                .agg(agg_func)
+
+    sum_prev.columns = ["_".join(x) for x in sum_prev.columns.ravel()]
+
+    # free up gpu ram
+    del(prev)
+
+    print("merging train feats")
+
+    train_feat = train.drop('TARGET', axis=1) \
+        .merge(avg_bureau, how='left', left_on='SK_ID_CURR', right_on='SK_ID_CURR') \
+        .merge(sum_cc_balance, how='left', left_on='SK_ID_CURR', right_index=True) \
+        .merge(sum_payments, how='left', left_on='SK_ID_CURR', right_on='SK_ID_CURR') \
+        .merge(sum_pc_balance, how='left', left_on='SK_ID_CURR', right_index=True) \
+        .merge(sum_prev, how='left', left_on='SK_ID_CURR', right_index=True) \
+        .drop('SK_ID_CURR', axis=1)
+
+    train_feat['DAYS_EMPLOYED'] = train_feat.DAYS_EMPLOYED.map(lambda x: np.nan if x == 365243 else x)
+    train_feat['DAYS_EMPLOYED_PERC'] = np.sqrt(train_feat.DAYS_EMPLOYED / train_feat.DAYS_BIRTH)
+    train_feat['INCOME_CREDIT_PERC'] = train_feat.AMT_INCOME_TOTAL / train_feat.AMT_CREDIT
+    train_feat['INCOME_PER_PERSON'] = np.log1p(train_feat.AMT_INCOME_TOTAL / train_feat.CNT_FAM_MEMBERS)
+
+    print("writing out data")
+
+    ## Outputs - to write out
+    avg_bureau.to_parquet(path='data_eng/avg_bureau')
+    sum_cc_balance.to_parquet(path='data_eng/sum_cc_balance')
+    sum_payments.to_parquet(path='data_eng/sum_payments')
+    sum_pc_balance.to_parquet(path='data_eng/sum_pc_balance')
+    sum_prev.to_parquet(path='data_eng/sum_prev')
+
+
+if __name__ == '__main__':
+
+    ### Load datasets
+    print("loading data")
+
+    bureau_balance = dd.read_csv('data/bureau_balance.csv')
+    bureau = dd.read_csv('data/bureau.csv')
+    cc_balance = dd.read_csv('data/credit_card_balance.csv')
+    payments = dd.read_csv('data/installments_payments.csv')
+    pc_balance = dd.read_csv('data/POS_CASH_balance.csv')
+    prev = dd.read_csv('data/previous_application.csv')
+    train = dd.read_csv('data/application_train.csv')
+    test = dd.read_csv('data/application_test.csv')
+
+    print("starting processing")
+
+    feature_engineering(bureau_balance, bureau, cc_balance, payments, pc_balance,
+                        prev, train, test)
--- feature_engineering_dask.py
+++ feature_engineering_dask.py
@@ -1,11 +1,11 @@
-## Feature Engineering cu-dask
+## Feature Engineering using dask
 
 import time
 
-from dask.distributed import Client, wait
+from dask.distributed import Client
 from dask_cuda import LocalCUDACluster
 import dask_cudf
-import numpy as np
+from feature_engineering import feature_engineering
 
 cluster = LocalCUDACluster()
 client = Client(cluster)
@@ -21,64 +21,7 @@ prev = dask_cudf.read_csv('data/previous_application.csv')
 train = dask_cudf.read_csv('data/application_train.csv')
 test = dask_cudf.read_csv('data/application_test.csv')
 
-## aggregation functions for our groupings
-agg_func = ['mean', 'max', 'min', 'sum', 'std']
+feature_engineering(bureau_balance, bureau, cc_balance, payments, pc_balance,
+                        prev, train, test)
 
-## Build Average Bureau Balance
-avg_bbalance = bureau_balance.select_dtypes('number') \
-                .groupby('SK_ID_BUREAU').agg(agg_func)
-
-avg_bbalance.columns = ["_".join(x) for x in avg_bbalance.columns.ravel()]
-
-## Build sum Credit Card Balance
-sum_cc_balance = cc_balance.drop('SK_ID_PREV', axis=1) \
-                    .select_dtypes('number').groupby('SK_ID_CURR') \
-                    .agg(agg_func)
-
-sum_cc_balance.columns = ["_".join(x) for x in sum_cc_balance.columns.ravel()]
-
-## Build Avg Bureau table
-avg_bureau = bureau.merge(avg_bbalance, how='left', 
-                          left_on='SK_ID_BUREAU', 
-                          right_index=True)
-
-## Buld Payments
-sum_payments = payments.drop('SK_ID_PREV', axis=1)
-sum_payments['PAYMENT_PERC'] = sum_payments.AMT_PAYMENT / sum_payments.AMT_INSTALMENT
-sum_payments['PAYMENT_DIFF'] = sum_payments.AMT_INSTALMENT - sum_payments.AMT_PAYMENT
-sum_payments['DPD'] = sum_payments.DAYS_ENTRY_PAYMENT - sum_payments.DAYS_INSTALMENT
-sum_payments['DBD'] = sum_payments.DAYS_INSTALMENT - sum_payments.DAYS_ENTRY_PAYMENT
-sum_payments['DPD'] = sum_payments['DPD']
-sum_payments['DBD'] = sum_payments['DBD']
-
-## Build Sum_PC_Balance
-sum_pc_balance = pc_balance.drop('SK_ID_PREV', axis=1).select_dtypes('number').groupby('SK_ID_CURR') \
-            .agg(agg_func)
-
-sum_pc_balance.columns = ["_".join(x) for x in sum_pc_balance.columns.ravel()]
-
-## Build Sum_Prev
-prev = prev.drop('SK_ID_PREV', axis=1)
-prev.DAYS_FIRST_DRAWING = prev.DAYS_FIRST_DRAWING.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_FIRST_DUE = prev.DAYS_FIRST_DUE.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_LAST_DUE_1ST_VERSION = prev.DAYS_LAST_DUE_1ST_VERSION.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_LAST_DUE = prev.DAYS_LAST_DUE.map(lambda x: np.nan if x == 365243 else x)
-prev.DAYS_TERMINATION = prev.DAYS_TERMINATION.map(lambda x: np.nan if x == 365243 else x)
-prev.APP_CREDIT_PERC = prev.AMT_APPLICATION / prev.AMT_CREDIT
-
-sum_prev = prev.select_dtypes('number').groupby('SK_ID_CURR') \
-            .agg(agg_func)
-
-sum_prev.columns = ["_".join(x) for x in sum_prev.columns.ravel()]
-
-
-## Outputs - to write out
-## Interesting... this creates a folder then a file in each
-avg_bureau.to_parquet(path='data_eng/avg_bureau')
-sum_cc_balance.to_parquet(path='data_eng/sum_cc_balance')
-sum_payments.to_parquet(path='data_eng/sum_payments')
-sum_pc_balance.to_parquet(path='data_eng/sum_pc_balance')
-sum_prev.to_parquet(path='data_eng/sum_prev')
-
-## we still ran out of memory
-
+client.close()
