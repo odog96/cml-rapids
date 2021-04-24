80116978147d315567cf48637a7d1b2fbebebb17
Data-drone <bpl.law@gmail.com>
2021-04-25 00:27:55 +1000

    updating to no longer spit out the intermediate frames to save memory for dummies post processing

--- feature_engineering_cudf.py
+++ feature_engineering_cudf.py
@@ -14,19 +14,20 @@ test = dd.read_parquet('raw_data/test.parquet')
 train_target = train['TARGET']
 unified = dd.concat([train.drop('TARGET', axis=1), test])
 print("starting processing")
-avg_bureau, sum_cc_balance, sum_payments, \
-    sum_pc_balance, sum_prev, unified_feat = feature_engineering(bureau_balance, bureau, 
-                                                cc_balance, payments, pc_balance,
-                                                prev, unified)
+
+unified_feat = feature_engineering(bureau_balance, bureau, 
+                                    cc_balance, payments, pc_balance,
+                                    prev, unified, dd)
+
 train_rows = train.shape
 train_feats = unified_feat.iloc[:307511].merge(train_target, how='left', 
                                            left_index=True, right_index=True)
 test_feats = unified_feat.iloc[307511:]
-avg_bureau.to_parquet(path='data_eng/avg_bureau')
-sum_cc_balance.to_parquet(path='data_eng/sum_cc_balance')
-sum_payments.to_parquet(path='data_eng/sum_payments')
-sum_pc_balance.to_parquet(path='data_eng/sum_pc_balance')
-sum_prev.to_parquet(path='data_eng/sum_prev')
+#avg_bureau.to_parquet(path='data_eng/avg_bureau')
+#sum_cc_balance.to_parquet(path='data_eng/sum_cc_balance')
+#sum_payments.to_parquet(path='data_eng/sum_payments')
+#sum_pc_balance.to_parquet(path='data_eng/sum_pc_balance')
+#sum_prev.to_parquet(path='data_eng/sum_prev')
 
 train_feats.to_parquet('data_eng/feats/train_feats.parquet')
 test_feats.to_parquet('data_eng/feats/test_feats.parquet')
--- feature_engineering_pandas.py
+++ feature_engineering_pandas.py
@@ -24,20 +24,19 @@ unified = dd.concat([train.drop('TARGET', axis=1), test])
 
 # need to split out the parquet writing
 # also need to fix a UserWarning: Pandas doesn't allow columns to be created via a new attribute name - see https://pandas.pydata.org/pandas-docs/stable/indexing.html#attribute-access
-avg_bureau, sum_cc_balance, sum_payments, \
-        sum_pc_balance, sum_prev, unified_feat = feature_engineering(bureau_balance, bureau, 
-                                                    cc_balance, payments, pc_balance,
-                                                    prev, unified)
+unified_feat = feature_engineering(bureau_balance, bureau, 
+                                cc_balance, payments, pc_balance,
+                                prev, unified, dd)
 
 train_rows = train.shape
 train_feats = unified_feat.iloc[:307511].merge(train_target, how='left', 
                                            left_index=True, right_index=True)
 test_feats = unified_feat.iloc[307511:]
 
-avg_bureau.to_parquet(path='data_eng/pandas/avg_bureau.parq')
-sum_cc_balance.to_parquet(path='data_eng/pandas/sum_cc_balance.parq')
-sum_payments.to_parquet(path='data_eng/pandas/sum_payments.parq')
-sum_pc_balance.to_parquet(path='data_eng/pandas/sum_pc_balance.parq')
-sum_prev.to_parquet(path='data_eng/pandas/sum_prev.parq')
+#avg_bureau.to_parquet(path='data_eng/pandas/avg_bureau.parq')
+#sum_cc_balance.to_parquet(path='data_eng/pandas/sum_cc_balance.parq')
+#sum_payments.to_parquet(path='data_eng/pandas/sum_payments.parq')
+#sum_pc_balance.to_parquet(path='data_eng/pandas/sum_pc_balance.parq')
+#sum_prev.to_parquet(path='data_eng/pandas/sum_prev.parq')
 train_feats.to_parquet('data_eng/feats/train_feats.parquet')
 test_feats.to_parquet('data_eng/feats/test_feats.parquet')
