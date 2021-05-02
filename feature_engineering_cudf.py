48f0878aacfe7ea82600de85b2b8f5605e29d7e7
Data-drone <bpl.law@gmail.com>
2021-05-02 10:39:35 +1000

    update both to use feature engineering 2

--- feature_engineering_cudf.py
+++ feature_engineering_cudf.py
@@ -1,5 +1,5 @@
 import cudf as dd
-from feature_engineering_adv import feature_engineering
+from feature_engineering_2 import process_unified, process_bureau_and_balance, process_previous_applications
 
 ### Load datasets
 print("loading data")
@@ -11,23 +11,27 @@ pc_balance = dd.read_parquet('raw_data/pc_balance.parquet')
 prev = dd.read_parquet('raw_data/prev.parquet')
 train = dd.read_parquet('raw_data/train.parquet')
 test = dd.read_parquet('raw_data/test.parquet')
+
+train_index = train.index
+test_index = test.index
+
 train_target = train['TARGET']
 unified = dd.concat([train.drop('TARGET', axis=1), test])
 print("starting processing")
 
-unified_feat = feature_engineering(bureau_balance, bureau, 
-                                    cc_balance, payments, pc_balance,
-                                    prev, unified, dd)
+unified_feat = process_unified(unified, dd)
 
-train_rows = train.shape
-train_feats = unified_feat.iloc[:307511].merge(train_target, how='left', 
-                                           left_index=True, right_index=True)
-test_feats = unified_feat.iloc[307511:]
-#avg_bureau.to_parquet(path='data_eng/avg_bureau')
-#sum_cc_balance.to_parquet(path='data_eng/sum_cc_balance')
-#sum_payments.to_parquet(path='data_eng/sum_payments')
-#sum_pc_balance.to_parquet(path='data_eng/sum_pc_balance')
-#sum_prev.to_parquet(path='data_eng/sum_prev')
+bureau_agg = process_bureau_and_balance(bureau, bureau_balance, dd)
+
+unified_feat = unified_feat.join(bureau_agg, how='left', on='SK_ID_CURR')
+
+# we can't use bool column types in xgb later on
+bool_columns = [col for col in unified_feat.columns if (unified_feat[col].dtype in ['bool']) ]    
+unified_feat[bool_columns] = unified_feat[bool_columns].astype('int64')
+
+train_feats = unified_feat.loc[train_index].merge(train_target, how='left', 
+                                               left_index=True, right_index=True)
+test_feats = unified_feat.loc[test_index]
 
 train_feats.to_parquet('data_eng/feats/train_feats.parquet')
 test_feats.to_parquet('data_eng/feats/test_feats.parquet')
--- feature_engineering_pandas.py
+++ feature_engineering_pandas.py
@@ -3,7 +3,7 @@
 import time
 
 import pandas as dd
-from feature_engineering_adv import feature_engineering
+from feature_engineering_2 import process_unified, process_bureau_and_balance, process_previous_applications
 
 ### Load Data
 bureau_balance = dd.read_parquet('raw_data/bureau_balance.parquet')
@@ -19,24 +19,28 @@ prev = dd.read_parquet('raw_data/prev.parquet')
 train = dd.read_parquet('raw_data/train.parquet')
 test = dd.read_parquet('raw_data/test.parquet')
 
+train_index = train.index
+test_index = test.index
+
 train_target = train['TARGET']
 unified = dd.concat([train.drop('TARGET', axis=1), test])
 
 # need to split out the parquet writing
 # also need to fix a UserWarning: Pandas doesn't allow columns to be created via a new attribute name - see https://pandas.pydata.org/pandas-docs/stable/indexing.html#attribute-access
-unified_feat = feature_engineering(bureau_balance, bureau, 
-                                cc_balance, payments, pc_balance,
-                                prev, unified, dd)
 
-train_rows = train.shape
-train_feats = unified_feat.iloc[:307511].merge(train_target, how='left', 
-                                           left_index=True, right_index=True)
-test_feats = unified_feat.iloc[307511:]
+unified_feat = process_unified(unified, dd)
 
-#avg_bureau.to_parquet(path='data_eng/pandas/avg_bureau.parq')
-#sum_cc_balance.to_parquet(path='data_eng/pandas/sum_cc_balance.parq')
-#sum_payments.to_parquet(path='data_eng/pandas/sum_payments.parq')
-#sum_pc_balance.to_parquet(path='data_eng/pandas/sum_pc_balance.parq')
-#sum_prev.to_parquet(path='data_eng/pandas/sum_prev.parq')
+bureau_agg = process_bureau_and_balance(bureau, bureau_balance, dd)
+
+unified_feat = unified_feat.join(bureau_agg, how='left', on='SK_ID_CURR')
+
+# we can't use bool column types in xgb later on
+bool_columns = [col for col in unified_feat.columns if (unified_feat[col].dtype in ['bool']) ]    
+unified_feat[bool_columns] = unified_feat[bool_columns].astype('int64')
+
+train_feats = unified_feat.loc[train_index].merge(train_target, how='left', 
+                                               left_index=True, right_index=True)
+test_feats = unified_feat.loc[test_index]
+
 train_feats.to_parquet('data_eng/feats/train_feats.parquet')
 test_feats.to_parquet('data_eng/feats/test_feats.parquet')
